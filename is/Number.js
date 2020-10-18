0450b090087151ea8bf9d73eae545f5e66fde934
rmcinto <rodger@geekshake.com>
2020-10-18 11:04:23 -0600

    [minor] Added new feature is_number

---
+++ is/Number.js
@@ -0,0 +1,9 @@
+/**
+* Checks to see if the value is a number
+* @function
+*   @param {any} v The value to test
+*   @returns {boolean}
+*/
+function isNumber(v) {
+    return typeof v === "number";
+}
