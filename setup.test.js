17f3a9e28cc9ad2a6fdc94a2022f47cbf8651f54
rmcinto <rodger@geekshake.com>
2020-12-12 17:34:14 -0700

    [minor] Updated manifests and dtrees

---
+++ setup.test.js
@@ -0,0 +1,50 @@
+/**
+* @test
+*   @type setup
+*   @name controller
+*/
+async function setupCoreController(
+    $entry
+    , $client
+    , $import
+    , $global
+    , $reporter
+) {
+    try {
+        var mocks = {
+            "fs": {}
+            , "path": {}
+        }
+        , controller = await $import(
+            "controller"
+            , mocks
+        )
+        , container = await $import(
+            "app"
+        )
+        , dtree = await $import(
+            "app1"
+        )
+        ;
+
+        controller
+            .setup
+            .setContainer(container)
+            .setAbstractTree(dtree)
+            .setGlobal($global)
+        ;
+
+        controller
+            .dependency
+            .add(
+                ".reporter"
+                , $reporter
+            )
+        ;
+
+        return $global.Promise.resolve(controller);
+    }
+    catch(ex) {
+        return $global.Promise.reject(ex);
+    }
+}
